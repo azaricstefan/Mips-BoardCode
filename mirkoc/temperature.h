89bc4109b076e8e37236e5c739f2c21e23c1154f
Mihailo Petric <pm130681d@student.etf.rs>
2017-08-24 21:56:47 +0200

    Dodato menjanje preciznosti merenja senzora temperature.

--- mirkoc/temperature.h
+++ mirkoc/temperature.h
@@ -14,7 +14,18 @@
 #define   _TEMP_READ_ZERO         65
 #define   _TEMP_READ_INIT         5
 #define   _TEMP_READ_WAIT         55
-#define   _WAIT_FOR_INIT_TIME     750
+#define   _WAIT_FOR_MESURING_x2     100
+#define   _WAIT_FOR_MESURING_x4     200
+#define   _WAIT_FOR_MESURING_x8     400
+#define   _WAIT_FOR_MESURING_x16     750
+#define   _DOUBLE_PRECISION	  2
+#define   _FOUR__PRECISION	      4   
+#define   _EIGHT_PRECISION	      8
+#define   _SIXTEEN_PRECISION	 16
+#define   _DOUBLE_PRECISION_CODED 0x1F
+#define   _FOUR_PRECISION_CODED 0x3F
+#define   _EIGHT_PRECISION_CODED 0x5F
+#define   _SIXTEEN_PRECISION_CODED 0x7F
 #define   _ERROR_TEMP             -100
 
 uint8_t oneWireReset();
@@ -22,5 +33,6 @@ void oneWireWrite(uint8_t byte);
 unsigned short oneWireRead();
 
 float calcTemp();
+void setPrecision(uint8_t precision_);
 
 #endif
